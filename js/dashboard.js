b9b2ffdbf89e70885d9524a222bc7bc3a71c77f6
AlejoNM2402 <alejonaranjom4@gmail.com>
2025-03-17 10:40:18 -0500

    feat: :memo: sisaw

---
+++ js/dashboard.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var logoutBtn = document.getElementById('logoutBtn');
+
+    logoutBtn.addEventListener('click', function() {
+        // Eliminar los datos de sesión del localStorage
+        localStorage.removeItem('usuarioId');
+        localStorage.removeItem('usuarioEmail');
+        localStorage.removeItem('isLoggedIn');
+
+        // Redirigir al usuario a la página de inicio de sesión
+        window.location.href = '../index.html'; // Ajusta la ruta según tu estructura de archivos
+    });
+
+    // Verificar si el usuario está logueado al cargar la página
+    function verificarSesion() {
+        var isLoggedIn = localStorage.getItem('isLoggedIn');
+        
+        if (isLoggedIn !== 'true') {
+            // Si no hay sesión iniciada, redirigir al login
+            window.location.href = '../index.html'; // Ajusta la ruta según tu estructura de archivos
+        }
+    }
+
+    verificarSesion();
+});
